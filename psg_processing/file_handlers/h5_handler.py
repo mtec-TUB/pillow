2bdeb4f022d83c8f5668856f930881212fe74254
lbeland <beland@campus.tu-berlin.de>
2026-01-20 15:00:39 +0100

    clean up h5 reader

--- psg_processing/file_handlers/h5_handler.py
+++ psg_processing/file_handlers/h5_handler.py
@@ -9,76 +9,53 @@ from .base import FileHandler
 class H5Handler(FileHandler):
     """Handler for H5 files."""
 
-    def collect_h5_dataset(self, name, obj, dataset):
-        """
-        Helper function to collect H5 datasets during file iteration.
-
-        Args:
-            name: Dataset name
-            obj: H5 object
-            dataset: List to append dataset information to
-        """
-        if isinstance(obj, h5py.Dataset):
-            dataset.append((name, obj[:]))
-
     def get_channels(self, logger, filepath):
-        """Extract channel names from H5 files."""
+        """Extract channel names (datasets) from H5 files."""
         try:
+            channel_names = []
             with h5py.File(filepath, "r") as f:
-                dataset = []
-                f.visititems(
-                    lambda name, obj: self.collect_h5_dataset(name, obj, dataset)
-                )
-                return [data[0] for data in dataset]
+                
+                def visitor(name, obj):
+                    if isinstance(obj, h5py.Dataset):
+                        channel_names.append(name)
+
+                f.visititems(visitor)
+            return channel_names
         except Exception as e:
-            logger.error(f"Error reading H5 file {filepath}: {e}")
-            return []
+                        logger.error(f"Error reading H5 file {filepath}: {e}")
+                        return []
 
     def read_signal(self, logger, filepath, channel):
         """Read signal from H5 file for specific channel."""
         try:
             with h5py.File(filepath, "r") as f:
-                dataset = []
-                f.visititems(
-                    lambda name, obj: self.collect_h5_dataset(name, obj, dataset)
-                )
-                signal_labels = [data[0] for data in dataset]
-                if channel in signal_labels:
-                    select_ch_idx = signal_labels.index(channel)
-                    return dataset[select_ch_idx][1]
+                if channel not in f:
+                    return None
+                
+                signal = f[channel][:]
+                return signal
         except Exception as e:
             logger.error(f"Error reading H5 signal from {filepath}: {e}")
-        return None
 
     def get_signal_data(self, logger, filepath, channel):
         """Get complete H5 signal information for processing."""
-        # DOD-O and DOD-H -specific sampling rate
-        sampling_rate = 250
         try:
-            with h5py.File(filepath, "r") as f:
-                dataset = []
-                f.visititems(
-                    lambda name, obj: self.collect_h5_dataset(name, obj, dataset)
-                )
-
-                ch_names = [data[0] for data in dataset]
-
-                if channel not in ch_names:
-                    self.logger.info(f"Channel {channel} not found")
-                    return None
-
-                select_ch_idx = ch_names.index(channel)
+            with h5py.File(filepath, "r") as f:                
+                dataset = f[channel]
+                signal = dataset[:]
                 logger.info(f"Channel selected: {channel}")
+                logger.info(f"Select channel samples: {len(signal)}")     
 
-                ch_samples = [data[1].shape[0] for data in dataset]
-                logger.info(f"Select channel samples: {ch_samples[select_ch_idx]}")
-                file_duration = ch_samples[select_ch_idx] / sampling_rate
-
-                signal = dataset[select_ch_idx][1]
+                # works for DOD-H and DOD-O
+                unit = dataset.parent.attrs.get("unit")   
+                sampling_rate = dataset.parent.attrs.get("fs")
+                
+                file_duration = len(signal) / sampling_rate
 
                 return {
                     "signal": signal,
                     "sampling_rate": sampling_rate,
+                    "unit": unit,
                     "start_datetime": None,
                     "file_duration": file_duration,
                 }
