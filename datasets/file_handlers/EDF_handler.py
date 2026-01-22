07a072291cf9f58bea6078477a46e3d7a7d36cc0
lbeland <beland@campus.tu-berlin.de>
2026-01-22 15:20:41 +0100

    try to use mne reader if pyedflib raises error

--- datasets/file_handlers/EDF_handler.py
+++ datasets/file_handlers/EDF_handler.py
@@ -3,6 +3,7 @@ EDF file handler for PSG data processing.
 """
 
 import pyedflib
+import mne
 
 
 class EDFHandler:
@@ -16,12 +17,17 @@ class EDFHandler:
                 # freqs = psg_f.getSampleFrequencies()
                 # return labels, freqs
                 return labels
-        except Exception as e:
-            logger.error(f"Error processing EDF file: {e}")
-            logger.error(
-                "Maybe the repair_edfs.py script or EDF Browser header repairer can help."
-            )
-            raise
+        except:
+            try:
+                raw = mne.io.read_raw_edf(filepath, preload=False, verbose='WARNING')
+                labels = raw.ch_names
+                return labels
+            except Exception as e:
+                logger.error(f"Error processing EDF file with pyedflib and mne: {e}")
+                logger.error(
+                    "Maybe the repair_edfs.py script or EDF Browser header repairer can help."
+                )
+                raise
 
     def read_signal(self, logger, filepath, channel):
         """Read signal from EDF file for specific channel."""
@@ -31,10 +37,14 @@ class EDFHandler:
                 if channel in ch_names_file:
                     ch_idx = ch_names_file.index(channel)
                     return psg_f.readSignal(ch_idx)
-        except Exception as e:
-            logger.error(f"Error reading EDF signal from: {e}")
-            logger.error("Maybe the repair_edfs.py script can help.")
-        return None
+        except:
+            try:
+                raw = mne.io.read_raw_edf(filepath, include=channel,preload=True, verbose='WARNING')
+                signal = raw.get_data()[0]
+                return signal
+            except Exception as e:
+                logger.error(f"Error reading EDF signal from: {e}")
+                logger.error("Maybe the repair_edfs.py script can help.")
 
     def get_signal_data(self, logger, filepath, channel):
         """Get complete EDF signal information for processing."""
@@ -59,16 +69,27 @@ class EDFHandler:
 
             psg_f.close()
 
-            return {
-                "signal": signal,
-                "sampling_rate": sampling_rate,
-                "unit": unit,
-                "start_datetime": start_datetime,
-                "file_duration": file_duration,
-            }
-        except Exception as e:
-            logger.error(f"Error processing EDF file: {e}")
-            logger.error(
-                "Maybe the repair_edfs.py script or EDF Browser header repairer can help."
-            )
-            raise
+        except:
+            try:
+                raw = mne.io.read_raw_edf(filepath, include=channel,preload=False, verbose='WARNING')
+                signal = raw.get_data()[0]
+                start_datetime = raw.info['meas_date']
+                sampling_rate = raw.info['sfreq']
+                file_duration = raw.n_times / sampling_rate
+                unit = raw.info['chs'][0]['unit']
+                unit = mne._fiff.meas_info._unit2human[unit]
+
+            except Exception as e:
+                logger.error(f"Error processing EDF file with pyedflib and mne: {e}")
+                logger.error(
+                    "Maybe the repair_edfs.py script or EDF Browser header repairer can help."
+                )
+                raise
+
+        return {
+            "signal": signal,
+            "sampling_rate": sampling_rate,
+            "unit": unit,
+            "start_datetime": start_datetime,
+            "file_duration": file_duration,
+        }
