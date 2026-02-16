097f1540c909abeec10dde1959d1ff3f08fc5768
lbeland <beland@campus.tu-berlin.de>
2026-02-16 16:40:25 +0100

    finalize eesm19

--- datasets/eesm19.py
+++ datasets/eesm19.py
@@ -4,6 +4,8 @@ from scipy.interpolate import interp1d
 import pandas as pd
 from decimal import Decimal
 from mne.io import read_raw_eeglab
+from pathlib import Path
+from typing import Dict, List, Tuple
 
 from psg_processing.core import Dataset_Explorer
 from datasets.base import BaseDataset
@@ -23,95 +25,190 @@ class EESM19(BaseDataset):
     
     def _setup_dataset_config(self):
         self.ann2label = {
-                        "Wake": 0,   # Wake
-                        "N1": 1,  # NREM Stage 1
-                        "N2": 2,  # NREM Stage 2
-                        "N3": 3,  # NREM Stage 3
-                        "REM": 4,   # REM sleep
-                        "Artefact": 6
+                        1: 0,   # Wake
+                        3: 1,  # NREM Stage 1
+                        4: 2,  # NREM Stage 2
+                        5: 3,  # NREM Stage 3
+                        2: 4,   # REM sleep
+                        7: 6    # Artefact
                         }
         
         
-        self.channel_names = ['F3', 'EMGl', 'F4', 'O2', 'O1', 'EMGr', 'EOGr', 'EMGc', 'C4', 'C3', 'M2', 'M1', 'EOGl']
+        self.channel_names = ['ERC', 'ELC', 'EOGl', 'ELA', 'O2', 'EMGr', 'EMGl', 'ELT', 'M1', 'C3', 'EOGr', 'O1', 'C4', 'ERA', 'EMGc', 'ERE', 
+                              'ERT', 'F4', 'ELE', 'ELI', 'ELB', 'ERI', 'ERB', 'M2', 'F3']
         
         
-        self.channel_types = {'analog': ['F3', 'EMGl', 'F4', 'O2', 'O1', 'EMGr', 'EOGr', 'EMGc', 'C4', 'C3', 'M2', 'M1', 'EOGl'], 'digital': []}
+        self.channel_types = {'analog': ['ELA', 'ELT', 'C3', 'ERC', 'EMGc', 'ERB', 'M2', 'ERE', 'M1', 'O1', 'ELB', 'EOGr', 'C4', 'O2', 'ELC', 
+                                         'F3', 'ERA', 'EMGl', 'EMGr', 'EOGl', 'ERT', 'ELE', 'ERI', 'ELI', 'F4'], 
+                              'digital': []}
         
         
-        self.channel_groups = {'eeg_eog': ['F3', 'F4', 'O2', 'O1', 'C4', 'C3', 'EOGr', 'EOGl','M2', 'M1'],
+        self.channel_groups = {'eeg_eog': ['ERC', 'ELC', 'EOGl', 'ELA', 'O2', 'ELT', 'M1', 'C3', 'EOGr', 'O1', 'C4', 'ERA', 'ERE', 
+                              'ERT', 'F4', 'ELE', 'ELI', 'ELB', 'ERI', 'ERB', 'M2', 'F3'],
                                 'emg': ['EMGl', 'EMGr', 'EMGc'],
                                 }
                 
         
-        self.file_extensions = {'psg_ext': '**/*_task-sleep_acq-PSG_eeg.set',
-                                'ann_ext': '**/*_task-sleep_acq-scoring_events.tsv'}
+        self.file_extensions = {'psg_ext': '**/*task-sleep_acq-*_eeg.set',  # include PSG and earEEG files, but exclude ‘auditory steady state responses’ (ASSR) and electrodeTestLab files
+                                'ann_ext': '**/*scoring1_events.tsv'} 
+        
     def dataset_paths(self) -> tuple[str, str]:
         return [
             self.dataset_name,
             self.dataset_name
         ]
     
-    def ann_parse(self, ann_fname, epoch_duration = None):
-        annot = pd.read_csv(ann_fname,sep='\t', header=0)
+    def get_file_identifier(self, psg_fname, ann_fname):
+        psg_id = Path(psg_fname).parent
+        ann_id = Path(ann_fname).parent
+        return psg_id, ann_id
+    
+    def ann_parse(self, ann_fname): 
 
-        ann_stage_events = []
-        start_time_label = None
+        base_fname = ann_fname.replace('scoring1_events.tsv','')
 
-        for i, row in annot.iterrows():
-            start = row['onset']
+        ann_stage_events_1 = []
+        ann_stage_events_2 = []
 
-            if start_time_label == None:
-                start_time_label = Decimal(str(start))
+        ann_fname1 = base_fname + 'scoring1_events.tsv'
+        if os.path.exists(ann_fname1):
+            annot = pd.read_csv(ann_fname1,sep='\t', header=0)
+            start_time_label = None
+            for i, row in annot.iterrows():
+                start = round(row['onset'])
 
-            duration = row['duration']
-            stage = row['scoring']
-            ann_stage_events.append({'Stage': stage,
-                                        'Start': float(Decimal(str(start)) - start_time_label),
-                                        'Duration': duration})
+                if start_time_label == None:
+                    start_time_label = start
+
+                duration = row['duration']
+                stage = row['Scoring1']
+                ann_stage_events_1.append({'Stage': stage,
+                                            'Start': start - start_time_label,
+                                            'Duration': duration})
             
 
-        return ann_stage_events, float(start_time_label)
+        ann_fname2 = base_fname + 'scoring2_events.tsv'
+        if os.path.exists(ann_fname2):
+            start_time_label = None
+            annot2 = pd.read_csv(ann_fname2,sep='\t', header=0)
+            for i, row in annot2.iterrows():
+                start = round(row['onset'])
+
+                if start_time_label == None:
+                    start_time_label = start
+
+                duration = row['duration']
+                stage = row['Scoring2']
+                ann_stage_events_2.append({'Stage': stage,
+                                            'Start': start - start_time_label,
+                                            'Duration': duration})
+            
+
+        return [ann_stage_events_1, ann_stage_events_2], start_time_label
     
-    # def align_front(self, logger, start_time, psg_fname, ann_fname, signal, labels, fs):
-    #     if not (start_time*fs).is_integer():
-    #         raise Exception("Annotations start at timestamp outside of sample rate")
+    def ann_label(self, logger, ann_stage_events: List[List[Dict]], epoch_duration: int) -> np.ndarray:
+        """
+        Convert multi-scorer sleep stage events to epoch-wise labels for ISRUC dataset.
+        Returns 2D array (n_epochs, n_scorers).
+        """
+        labels = [np.array([]), np.array([])]
 
-    #     if start_time > 0:
-    #         logger.info(f"Labeling started {start_time/60:.2f} min after signal start, signal will be shortened at the front to match")
-    #         signal = signal[int(start_time*fs):]
+        for i, annotation in enumerate(ann_stage_events):  # two scorers
+            total_duration = 0
+            for event in annotation:
+                onset_sec = int(event['Start'])
+                duration_sec = int(event['Duration'])
+                ann_str = event['Stage']
 
-    #     return True, signal, labels
+                # Sanity check
+                assert onset_sec == total_duration, f"Onset sec of epoch is {onset_sec} but last epoch ended at {total_duration}"
+
+                # Get label value
+                if ann_str in self.ann2label:
+                    label = self.ann2label[ann_str]
+                else:
+                    logger.info(f"Something unexpected: label {ann_str} not found")
+                    raise Exception(f"Something unexpected: label {ann_str} not found")
+
+                # Compute # of epoch for this stage
+                if duration_sec % epoch_duration != 0:
+                    logger.info(f"Something wrong: {duration_sec} {epoch_duration}")
+                    raise Exception(f"Something wrong: {duration_sec} {epoch_duration}")
+                duration_epoch = int(duration_sec / epoch_duration)
+
+                # Generate sleep stage labels
+                label_epoch = np.ones(duration_epoch, dtype=np.int32) * label
+                labels[i] = np.append(labels[i], label_epoch)
+
+                total_duration += duration_sec
+
+                # logger.info("Include onset:{}, duration:{}, label:{} ({})".format(
+                #     onset_sec, duration_sec, label, ann_str
+                # ))
+
+        # Pad shorter annotation to match longer one
+        if len(labels[0]) != len(labels[1]):
+            max_len = max(len(labels[0]), len(labels[1]))
+            labels[0] = np.pad(labels[0], (0, max_len - len(labels[0])), mode='constant', constant_values=6)
+            labels[1] = np.pad(labels[1], (0, max_len - len(labels[1])), mode='constant', constant_values=6)
+
+        labels = np.array(labels).T  # Transpose to (n_epochs, n_scorers)
+        
+        return labels
     
-    # def align_end(self, logger, psg_fname, ann_fname, signals, labels):
+    def align_front(self, logger, alignment, pad_values, epoch_duration, delay_sec, signal, labels, fs):
+        logger.info("Alignment of scorer 1")
+        signal1, labels1 = self.base_align_front(logger, delay_sec, alignment, pad_values, epoch_duration, signal, labels[:,0],fs) 
+        logger.info("Alignment of scorer 2")
+        signal2, labels2 = self.base_align_front(logger, delay_sec, alignment, pad_values, epoch_duration, signal, labels[:,1],fs)
 
-    #     if len(labels) == len(signals) +1:
-    #         logger.info(f"Labels (len: {len(labels)}) are shortend to match signal ({len(signals)})")
-    #         labels = labels[:len(signals)]
-
-    #     if len(signals) > len(labels):
-    #         logger.info(f"Signal (len: {len(signals)}) is shortend to match label (len: {len(labels)})")
-    #         signals = signals[:len(labels)]
-
-    #     assert len(signals) == len(labels), f"Length mismatch: signal ({os.path.basename(psg_fname)})={len(signals)}, labels({os.path.basename(ann_fname)})={len(labels)} TODO: implement alignment function"
-
-    #     return signals, labels
+        assert len(signal1) == len(signal2), f"Length mismatch after front alignment: signal1={len(signal1)}, signal2={len(signal2)}"
+        assert len(labels1) == len(labels2), f"Length mismatch after front alignment: labels1={len(labels1)}, labels2={len(labels2)}"
+        
+        return signal1, np.array([labels1, labels2]).T  # Return (signal, (n_epochs, n_scorers))
     
+    def align_end(self, logger, alignment, pad_values, psg_fname, ann_fname, signals, labels):
+
+        if len(labels) == len(signals) + 1:
+            logger.info("Alignment of scorer 1")
+            signals1, labels1 = self.base_align_end_labels_longer(logger, alignment, pad_values, signals, labels[:,0])
+            logger.info("Alignment of scorer 2")
+            signals2, labels2 = self.base_align_end_labels_longer(logger, alignment, pad_values, signals, labels[:,1])
+            assert len(signals1) == len(signals2), f"Length mismatch after end alignment: signals1={len(signals1)}, signals2={len(signals2)}"
+            assert len(labels1) == len(labels2), f"Length mismatch after end alignment: labels1={len(labels1)}, labels2={len(labels2)}"
+            return signals1, np.array([labels1, labels2]).T  # Return (signals, (n_epochs, n_scorers))
+
+        if len(signals) > len(labels):
+            logger.info("Alignment of scorer 1")
+            signals1, labels1 = self.base_align_end_signals_longer(logger, alignment, pad_values, signals, labels[:,0])
+            logger.info("Alignment of scorer 2")
+            signals2, labels2 = self.base_align_end_signals_longer(logger, alignment, pad_values, signals, labels[:,1])
+            assert len(signals1) == len(signals2), f"Length mismatch after end alignment: signals1={len(signals1)}, signals2={len(signals2)}"
+            assert len(labels1) == len(labels2), f"Length mismatch after end alignment: labels1={len(labels1)}, labels2={len(labels2)}"
+            return signals1, np.array([labels1, labels2]).T  # Return (signals, (n_epochs, n_scorers))
+
     def preprocess(self, data_dir, ann_dir, output_dir):
-        print("\n EESM23 files originally contain NaN values in signals. \n")
+        return EESM_Preprocessor(self).preprocess(data_dir, ann_dir, output_dir)
+
+class EESM_Preprocessor:
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def preprocess(self, data_dir, ann_dir, output_dir):
+        print("\n Files originally contain NaN values in signals. \n")
         
         execute_preprocess = input("Do you want to interpolate over these NaN values now (as recommended from the dataset authors)? (Y/N) ")
         
         if str(execute_preprocess).lower() == "y":
 
             self.interpolate_files(data_dir, ann_dir, output_dir)
-
-            self.file_extensions['psg_ext'] = '**/*_task-sleep_acq-PSG_eeg_interpolated.set'
             
             if str(input("Do you want to continue with processing now? (Y/N) ")).lower() == "n":
                 return False
         
         if str(input("Do you want to use the interpolated files for processing (if existing)? (Y/N) ")).lower() == "y":
-            self.file_extensions['psg_ext'] = '**/*_task-sleep_acq-PSG_eeg_interpolated.set'
+            self.dataset.file_extensions['psg_ext'] = self.dataset.file_extensions['psg_ext'].replace('*.set','*_interpolated.set')
         return True
     
 
@@ -119,7 +216,7 @@ class EESM19(BaseDataset):
         # Get files using dataset-specific extensions
         explorer = Dataset_Explorer(
             logger=None,
-            psg_file_handler=None,
+            dataset = self.dataset,
             data_dir=data_dir,
             ann_dir=ann_dir
         )
@@ -128,8 +225,18 @@ class EESM19(BaseDataset):
         # Process each file
         for psg_idx, psg_fname in enumerate(psg_fnames):
             print(f"\n--- Preprocessing file {psg_idx+1}/{len(psg_fnames)} ---")
+
+            path,ext = os.path.splitext(psg_fname)
+            output_path = os.path.join(path + '_interpolated' + ext)
+            if os.path.exists(output_path):
+                print(f"Interpolated file already exists: {output_path}, skipping interpolation for this file.")
+                continue
             
-            raw_data = read_raw_eeglab(psg_fname, verbose=False, preload=True)
+            try:
+                raw_data = read_raw_eeglab(psg_fname, verbose=False, preload=True)
+            except Exception as e:
+                print(f"Error reading {psg_fname} with MNE: {e}. Skipping interpolation for this file.")
+                continue
 
             signals = raw_data.get_data()
             fs = raw_data.info['sfreq']
@@ -137,9 +244,8 @@ class EESM19(BaseDataset):
             signals_interpolated = self.interpolateOverNans(signals, fs)
                 
             raw_data._data= signals_interpolated
-            path,ext = os.path.splitext(psg_fname)
-            raw_data.export(os.path.join(path + '_interpolated' + ext), fmt='eeglab', verbose=False,overwrite=True)
-            print(f"Interpolated file saved as {os.path.basename(path + '_interpolated' + ext)}")
+            raw_data.export(output_path, fmt='eeglab', verbose=False,overwrite=True)
+            print(f"Interpolated file saved as {output_path}")
 
 
     def findRuns(self,input,noWarning=False):
