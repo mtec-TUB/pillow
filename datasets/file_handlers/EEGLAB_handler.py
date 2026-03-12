122414b4f8be61e323b75b782aad8a1fc92b1db6
lbeland <beland@campus.tu-berlin.de>
2026-03-12 11:06:11 +0100

    more robust loading of eeglab files (handle epoched files)

--- datasets/file_handlers/EEGLAB_handler.py
+++ datasets/file_handlers/EEGLAB_handler.py
@@ -1,4 +1,5 @@
 import mne
+import numpy as np
 from pymatreader import read_mat
 
 class EEGLABHandler:
@@ -16,8 +17,11 @@ class EEGLABHandler:
         except:
             # Fall back to slower mne that may preload the data
             try:
-                raw_data = mne.io.read_raw_eeglab(filepath, verbose="WARNING")
+                raw_data = mne.io.read_raw_eeglab(filepath, verbose="WARNING",preload=False)
                 return raw_data.ch_names
+            except TypeError as e:
+                raw_epoched_data = mne.io.read_epochs_eeglab(filepath, verbose="WARNING")
+                return raw_epoched_data.ch_names
             except Exception as e:
                 logger.error(f"Error during channel extraction from {filepath}: {e}")
                 raise
@@ -30,9 +34,27 @@ class EEGLABHandler:
                 return raw_data.get_data(picks=channel)[0]
             else:
                 return None # channel not found in this file
+        except TypeError as e:
+            # Probably because its an epoched file -> reading as epochs and concatenating
+            # First load with mat to get channel names and n_epochs (needed to construct events for mne)
+            eeg = read_mat(filepath)
+            eeg = eeg.get("EEG", eeg)   # handle nested structure
+            chanlocs = eeg.get("chanlocs")
+            labels = chanlocs.get('labels',[]) 
+            if channel in labels:
+                n_epochs = eeg.get("trials")
+                events = np.vstack((np.arange(n_epochs,dtype=int), np.zeros(n_epochs,dtype=int), np.ones(n_epochs, dtype=int))).T
+                event_id=dict(unknown=1)
+                raw_epoched_data = mne.io.read_epochs_eeglab(filepath, verbose="WARNING", events=events, event_id=event_id)
+            
+                epoched_data = raw_epoched_data.get_data(picks=channel)[:,0,:]  # shape (n_epochs, n_times)
+                continuous = epoched_data.flatten()
+                return continuous
+            else:
+                return None
         except Exception as e:
             logger.error(f"Error reading signal from {filepath}: {e}")
-        
+            return None
 
     def get_signal_data(self, logger, filepath, channel):
         """Get complete signal information for specific channel."""
@@ -40,24 +62,41 @@ class EEGLABHandler:
             raw_data = mne.io.read_raw_eeglab(filepath, verbose='WARNING', preload=True)
 
             signal = raw_data.get_data(picks=channel)[0]
-            samples = raw_data.n_times
-            assert samples == len(signal)
+            samples = len(signal)
+            info = raw_data.info
 
-            sampling_rate = raw_data.info["sfreq"]
-            file_duration = samples / sampling_rate
-            assert file_duration == raw_data.duration
+        except TypeError as e:
+            # Probably because its an epoched file -> reading as epochs and concatenating
+            # First load with mat to get channel names and n_epochs (needed to construct events for mne)
+            eeg = read_mat(filepath)
+            eeg = eeg.get("EEG", eeg)   # handle nested structure
 
-            start_datetime = raw_data.info["meas_date"]
-
-            unit = raw_data.info['chs'][raw_data.info['ch_names'].index(channel)]['unit']
-            unit = mne._fiff.meas_info._unit2human[unit]
-            return {
-                "signal": signal,
-                "sampling_rate": sampling_rate,
-                "unit": unit,
-                "start_datetime": start_datetime,
-                "file_duration": file_duration,
-            }
+            # Construct events because mne cannot handle epoched .set files without events
+            n_epochs = eeg.get("trials")
+            events = np.vstack((np.arange(n_epochs,dtype=int), np.zeros(n_epochs,dtype=int), np.ones(n_epochs, dtype=int))).T
+            event_id=dict(unknown=1)
+            raw_epoched_data = mne.io.read_epochs_eeglab(filepath, verbose="WARNING", events=events, event_id=event_id)
+        
+            epoched_data = raw_epoched_data.get_data(picks=channel)[:,0,:]  # shape (n_epochs, n_times)
+            
+            signal = epoched_data.flatten()
+            samples = len(signal)
+            info = raw_epoched_data.info
+        
         except Exception as e:
             logger.error(f"Error during data retrieval {filepath}: {e}")
             raise
+
+
+        sampling_rate = info["sfreq"]
+        file_duration = samples / sampling_rate
+        start_datetime = info["meas_date"]
+        unit = info['chs'][info['ch_names'].index(channel)]['unit']
+        unit = mne._fiff.meas_info._unit2human[unit]
+        return {
+            "signal": signal,
+            "sampling_rate": sampling_rate,
+            "unit": unit,
+            "start_datetime": start_datetime,
+            "file_duration": file_duration,
+        }
