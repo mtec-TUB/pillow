465d95b420efa10110986cb477d9da6f25e044e1
lbeland <beland@campus.tu-berlin.de>
2026-05-27 15:05:19 +0200

    support reading of .rec files with mne (requieres mne>=1.10)

--- datasets/file_handlers/EDF_handler.py
+++ datasets/file_handlers/EDF_handler.py
@@ -1,5 +1,7 @@
 import os
 import mne
+from mne.io import read_raw_edf
+from mne import _fiff
 from warnings import catch_warnings
 
 class EDFHandler:
@@ -10,7 +12,7 @@ class EDFHandler:
         """Extract channel names from file."""
         try:
             with catch_warnings(record=True) as w:  # to handle duplicate channel names
-                raw = mne.io.read_raw_edf(filepath, preload=False, verbose='WARNING')
+                raw = read_raw_edf(filepath, preload=False, verbose='WARNING')
                 if w:
                     if w[0].message.args[0].startswith("Channel names are not unique"):
                         # duplicate channel names detected -> mne automatically maps them two unique names
@@ -19,7 +21,7 @@ class EDFHandler:
                         duplicates = []
                         for ch in raw.ch_names:
                             # try to load the file including only this channel name
-                            dupl_test_raw = mne.io.read_raw_edf(filepath, include=ch,preload=False, verbose='WARNING')
+                            dupl_test_raw = read_raw_edf(filepath, include=ch,preload=False, verbose='WARNING')
                             if ch not in dupl_test_raw.ch_names:
                                 # Channel not found because it is one of the mapped unique names
                                 duplicates.append(ch)
@@ -32,21 +34,28 @@ class EDFHandler:
                 else:
                     labels = raw.ch_names
             return labels
+        except NotImplementedError as e:
+            # This can happen for some EDF files which e.g. do not have the standard .edf extension, but are still internally in EDF format (.rec)
+            try:
+                with open(filepath, "rb") as f:
+                    raw = read_raw_edf(f, preload=True, verbose="WARNING")   # only supported for mne version >= 1.10
+                    labels = raw.ch_names
+                    return labels
+            except Exception as e:
+                raise
         except KeyboardInterrupt:
             # Always re-raise KeyboardInterrupt
             raise
         except Exception as e:
             logger.error(f"Error during channel extraction: {e}")
-            logger.error(
-                "Maybe EDF Browser header repair can help."
-            )
+            logger.error("Maybe EDF Browser header repair can help.")
             return []
 
     def read_signal(self, logger, filepath, channel):
         """Read signal from file for specific channel."""
         try:
             with catch_warnings(record=True) as w:
-                raw = mne.io.read_raw_edf(filepath, include=channel,preload=True, verbose='WARNING')
+                raw = read_raw_edf(filepath, include=channel,preload=True, verbose='WARNING')
                 if w:
                     if w[0].message.args[0].startswith("Channel names are not unique"):
                         channel = raw.ch_names[0]   # take only the first of the duplicate channels
@@ -55,22 +64,37 @@ class EDFHandler:
             if channel in raw.ch_names:
                 signal = raw.get_data()[0]
                 return signal
+        except NotImplementedError as e:
+            # This can happen for some EDF files which e.g. do not have the standard .edf extension, but are still internally in EDF format (.rec)
+            try:
+                with open(filepath, "rb") as f:
+                    raw = read_raw_edf(f, include=channel, preload=True, verbose="WARNING")   # only supported for mne version >= 1.10
+                    if channel in raw.ch_names:
+                        signal = raw.get_data()[0]
+                        return signal
+            except Exception as e:
+                raise
         except KeyboardInterrupt:
             # Always re-raise KeyboardInterrupt
             raise
         except Exception as e:
             logger.error(f"Error during signal extraction: {e}")
-            logger.error(
-                "Maybe EDF Browser header repair can help."
-            )
+            logger.error("Maybe EDF Browser header repair can help.")
         return None     # channel was not found in this file
         
     def get_start_datetime(self, logger, filepath):
         """Get start datetime of file."""
         try:
-            raw = mne.io.read_raw_edf(filepath, preload=False, verbose='WARNING')                
+            raw = read_raw_edf(filepath, preload=False, verbose='WARNING')                
             start_datetime = raw.info['meas_date']
-
+        except NotImplementedError as e:
+            # This can happen for some EDF files which e.g. do not have the standard .edf extension, but are still internally in EDF format (.rec)
+            try:
+                with open(filepath, "rb") as f:
+                    raw = read_raw_edf(f, preload=True, verbose="WARNING")   # only supported for mne version >= 1.10
+                    start_datetime = raw.info['meas_date']
+            except Exception as e:
+                raise
         except KeyboardInterrupt:
             # Always re-raise KeyboardInterrupt
             raise
@@ -85,7 +109,7 @@ class EDFHandler:
         """Get complete signal information for specific channel."""
         try:
             with catch_warnings(record=True) as w:
-                raw = mne.io.read_raw_edf(filepath, include=[channel], preload=False, verbose='WARNING')
+                raw = read_raw_edf(filepath, include=[channel], preload=False, verbose='WARNING')
                 if w:
                     if w[0].message.args[0].startswith("Channel names are not unique"):
                         channel = raw.ch_names[0]    # take only the first of the duplicate channels
@@ -96,8 +120,19 @@ class EDFHandler:
             sampling_rate = raw.info['sfreq']
             file_duration = raw.n_times / sampling_rate
             unit = raw.info['chs'][0]['unit']
-            unit = mne._fiff.meas_info._unit2human[unit]
-
+            unit = _fiff.meas_info._unit2human[unit]
+        except NotImplementedError as e:
+            # This can happen for some EDF files which e.g. do not have the standard .edf extension, but are still internally in EDF format (.rec)
+            try:
+                with open(filepath, "rb") as f:
+                    raw = read_raw_edf(f, include=[channel], preload=True, verbose="WARNING")   # only supported for mne version >= 1.10
+                    signal = raw.get_data(picks=channel)[0]
+                    sampling_rate = raw.info['sfreq']
+                    file_duration = raw.n_times / sampling_rate
+                    unit = raw.info['chs'][0]['unit']
+                    unit = _fiff.meas_info._unit2human[unit]
+            except Exception as e:
+                raise
         except KeyboardInterrupt:
             # Always re-raise KeyboardInterrupt
             raise
